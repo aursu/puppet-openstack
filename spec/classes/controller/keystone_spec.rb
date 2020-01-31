e4e4134dc188d817120d467843c31ef8d5ba424e
Alexander Ursu <alexander.ursu@gmail.com>
2020-01-31 12:58:14 +0100

    Added unit test to check different private API port setup for OpenStack Pike

--- spec/classes/controller/keystone_spec.rb
+++ spec/classes/controller/keystone_spec.rb
@@ -20,6 +20,31 @@ describe 'openstack::controller::keystone' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it {
+        is_expected.to contain_exec('keystone-manage-bootstrap')
+          .with_command(%r{--bootstrap-admin-url http://controller:5000/v3/})
+      }
+
+      context "when release cycle is before the Queens release" do
+        let(:pre_condition) do
+          <<-PRECOND
+          include openstack
+          openstack::repository { 'pike':
+          }
+          PRECOND
+        end
+        let(:params) do
+          super().merge(
+            'cycle' => 'pike',
+          )
+        end
+
+        it {
+          is_expected.to contain_exec('keystone-manage-bootstrap')
+            .with_command(%r{--bootstrap-admin-url http://controller:35357/v3/})
+        }
+      end
     end
   end
 end
