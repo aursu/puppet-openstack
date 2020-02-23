80b20417883f8c3c00601110165f1c45b423b105
Alexander Ursu <alexander.ursu@gmail.com>
2020-02-23 23:20:41 +0100

    Added rspec test for openstack::djangoconfig

--- spec/defines/djangoconfig_spec.rb
+++ spec/defines/djangoconfig_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'openstack::djangoconfig' do
-  let(:title) { 'namevar' }
+  let(:title) { '/etc/openstack-dashboard/local_settings' }
   let(:params) do
     {}
   end
@@ -9,8 +9,24 @@ describe 'openstack::djangoconfig' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) do
+        {
+          content: {
+            'OPENSTACK_HOST' => "'controller'",
+          },
+        }
+      end
 
       it { is_expected.to compile }
+
+      it {
+        is_expected.to contain_djangosetting('/etc/openstack-dashboard/local_settings/OPENSTACK_HOST')
+          .with(
+            ensure: :present,
+            config: '/etc/openstack-dashboard/local_settings',
+            value: "'controller'",
+          )
+      }
     end
   end
 end
