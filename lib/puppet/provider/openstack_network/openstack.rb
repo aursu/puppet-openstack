cd21682d448c394bbb899928327546be05b9ed25
Alexander Ursu <alexander.ursu@gmail.com>
2020-04-06 09:22:13 +0200

    Added openstack_network

---
+++ lib/puppet/provider/openstack_network/openstack.rb
@@ -0,0 +1,175 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'openstack'))
+
+Puppet::Type.type(:openstack_network).provide(:openstack, parent: Puppet::Provider::Openstack) do
+  desc 'Manage networks for OpenStack.'
+
+  commands openstack: 'openstack'
+
+  def initialize(value = {})
+    super(value)
+    @property_flush = {}
+  end
+
+  # Generates method for all properties of the property_hash
+  mk_resource_methods
+
+  def self.provider_subcommand
+    'network'
+  end
+
+  def self.provider_list
+    get_list(provider_subcommand)
+  end
+
+  def self.provider_create(*args)
+    openstack_caller(provider_subcommand, 'create', *args)
+  end
+
+  def self.provider_delete(*args)
+    openstack_caller(provider_subcommand, 'delete', *args)
+  end
+
+  def self.provider_set(*args)
+    openstack_caller(provider_subcommand, 'set', *args)
+  end
+
+  def self.instances
+    openstack_command
+
+    provider_list.map do |entity_name, entity|
+      router_type = (entity['router_type'].downcase == 'external')
+      enabled = (entity['state'].downcase == 'up')
+
+      new(name: entity_name,
+          ensure: :present,
+          id: entity['id'],
+          subnets: entity['subnets'],
+          external: router_type.to_s.to_sym,
+          shared: entity['shared'].to_s.to_sym,
+          description: entity['description'],
+          enabled: enabled.to_s.to_sym,
+          provider_network_type: entity['network_type'],
+          project: entity['project'],
+          provider: name)
+    end
+  end
+
+  def self.prefetch(resources)
+    entities = instances
+    # rubocop:disable Lint/AssignmentInCondition
+    resources.keys.each do |entity_name|
+      if provider = entities.find { |entity| entity.name == entity_name }
+        resources[entity_name].provider = provider
+      end
+    end
+    # rubocop:enable Lint/AssignmentInCondition
+  end
+
+  def create
+    name     = @resource[:name]
+    project  = @resource.value(:project)
+    shared   = @resource.value(:shared)
+    external = @resource.value(:external)
+    desc     = @resource.value(:description)
+    enabled  = @resource.value(:enabled)
+    provider_physical_network = @resource.value(:provider_physical_network)
+    provider_network_type     = @resource.value(:provider_network_type)
+
+    @property_hash[:project] = project if project
+    @property_hash[:shared] = shared
+    @property_hash[:external] = external
+    @property_hash[:description] = desc if desc
+    @property_hash[:enabled] = enabled
+    @property_hash[:provider_physical_network] = provider_physical_network if provider_physical_network
+    @property_hash[:provider_network_type] = provider_network_type if provider_network_type
+
+    args = []
+    args += ['--project', project] if project
+
+    args << '--share' if shared == :true
+    args << '--no-share' if shared == :false
+
+    args << '--external' if external == :true
+    args << '--internal' if external == :false
+
+    args += ['--description', desc] if desc
+
+    args << '--enable' if enabled == :true
+    args << '--disable' if enabled == :false
+
+    args += ['--provider-physical-network', provider_physical_network] if provider_physical_networ
+    args += ['--provider-network-type', provider_network_type] if provider_network_type
+
+    args << name
+
+    self.class.provider_create(*args)
+
+    @property_hash[:ensure] = :present
+  end
+
+  def destroy
+    name = @resource[:name]
+
+    self.class.provider_delete(name)
+
+    @property_hash.clear
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present || false
+  end
+
+  def project=(proj)
+    @property_flush[:project] = proj
+  end
+
+  def shared=(stat)
+    @property_flush[:shared] = stat
+  end
+
+  def external=(stat)
+    @property_flush[:external] = stat
+  end
+
+  def description=(desc)
+    @property_flush[:description] = desc
+  end
+
+  def enabled=(stat)
+    @property_flush[:enabled] = stat
+  end
+
+  def provider_network_type=(net_type)
+    @property_flush[:provider_network_type] = net_type
+  end
+
+  def flush
+    return if @property_flush.empty?
+    args = []
+    name     = @resource[:name]
+    project  = @resource.value(:project)
+    desc     = @resource.value(:description)
+    provider_network_type     = @resource.value(:provider_network_type)
+
+    args += ['--project', project] if @property_flush[:project]
+
+    args << '--share' if @property_flush[:shared] == :true
+    args << '--no-share' if @property_flush[:shared] == :false
+
+    args << '--external' if @property_flush[:external] == :true
+    args << '--internal' if @property_flush[:external] == :false
+
+    args += ['--description', desc] if @property_flush[:description]
+
+    args << '--enable' if @property_flush[:enabled] == :true
+    args << '--disable' if @property_flush[:enabled] == :false
+
+    args += ['--provider-network-type', provider_network_type] if provider_network_type
+
+    @property_flush.clear
+
+    return if args.empty?
+    args << name
+    self.class.provider_set(*args)
+  end
+end
---
+++ lib/puppet/type/openstack_network.rb
@@ -0,0 +1,92 @@
+Puppet::Type.newtype(:openstack_network) do
+  @doc = <<-PUPPET
+    @summary
+      A network is an isolated Layer 2 networking segment. There are two types
+      of networks, project and provider networks. Project networks are fully
+      isolated and are not shared with other projects. Provider networks map to
+      existing physical networks in the data center and provide external
+      network access for servers and other resources. Only an OpenStack
+      administrator can create provider networks. Networks can be connected via
+      routers.
+
+      https://docs.openstack.org/python-openstackclient/train/cli/command-objects/network.html
+    PUPPET
+
+  ensurable
+
+  newparam(:name, namevar: true) do
+    desc 'New network name'
+  end
+
+  newparam(:id) do
+    desc 'Network ID (read only)'
+  end
+
+  newparam(:subnets) do
+    desc 'Subnets (read only)'
+  end
+
+  newproperty(:project) do
+    desc 'Default project (name or ID)'
+    defaultto ''
+
+    def insync?(_is)
+      p = resource.project_instance(@should)
+      return false if p.nil?
+
+      true
+    end
+  end
+
+  newproperty(:enabled) do
+    desc 'Enable network (default)'
+
+    newvalues(:true, :false)
+    defaultto :true
+  end
+
+  newproperty(:shared) do
+    desc 'Share the network between projects'
+
+    newvalues(:true, :false)
+  end
+
+  newproperty(:external) do
+    desc 'Set this network as an external network (external-net extension required)'
+
+    newvalues(:true, :false)
+  end
+
+  newproperty(:description) do
+    desc 'Project description'
+  end
+
+  newproperty(:provider_physical_network) do
+    desc 'Name of the physical network over which the virtual network is implemented'
+  end
+
+  newproperty(:provider_network_type) do
+    desc <<-PUPPET
+      The physical mechanism by which the virtual network is implemented.
+      The supported options are: flat, geneve, gre, local, vlan, vxlan.
+    PUPPET
+
+    newvalues(:flat, :geneve, :gre, :local, :vlan, :vxlan)
+  end
+
+  autorequire(:openstack_project) do
+    rv = []
+    rv << self[:project] if self[:project]
+    rv
+  end
+
+  def project_instance(lookup_id)
+    lookup_id = lookup_id.is_a?(Array) ? lookup_id.first : lookup_id
+
+    instances = Puppet::Type.type(:openstack_project).instances
+                            .select { |resource| resource[:name] == lookup_id || resource[:id] == lookup_id }
+    return nil if instances.empty?
+    # no support for multiple OpenStack domains
+    instances.first
+  end
+end
