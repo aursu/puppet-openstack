a4923b2683f3df91a28575c6401508353c7b3e8c
Alexander Ursu <alexander.ursu@gmail.com>
2020-04-11 21:01:24 +0200

    Added resource and  instances methods lookup

--- lib/puppet_x/openstack/customtype.rb
+++ lib/puppet_x/openstack/customtype.rb
@@ -6,6 +6,51 @@ module CustomType
     end
   end
 
+  def project_instance(lookup_id)
+    lookup_id = lookup_id.is_a?(Array) ? lookup_id.first : lookup_id
+
+    instances = Puppet::Type.type(:openstack_project).instances
+                            .select { |resource| resource[:name] == lookup_id || resource[:id] == lookup_id }
+    return nil if instances.empty?
+    # no support for multiple OpenStack domains
+    instances.first
+  end
+
+  def project_resource(lookup_id)
+    lookup_id = lookup_id.is_a?(Array) ? lookup_id.first : lookup_id
+    catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:openstack_project)) && [r[:name], r[:id]].include?(lookup_id) }
+  end
+
+  def network_instance(lookup_id)
+    lookup_id = lookup_id.is_a?(Array) ? lookup_id.first : lookup_id
+
+    instances = Puppet::Type.type(:openstack_network).instances
+                            .select { |resource| resource[:name] == lookup_id || resource[:id] == lookup_id }
+
+    return nil if instances.empty?
+    instances.first
+  end
+
+  def network_resource(lookup_id)
+    lookup_id = lookup_id.is_a?(Array) ? lookup_id.first : lookup_id
+    catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:openstack_network)) && [r[:name], r[:id]].include?(lookup_id) }
+  end
+
+  def subnet_instance(lookup_id)
+    lookup_id = lookup_id.is_a?(Array) ? lookup_id.first : lookup_id
+
+    instances = Puppet::Type.type(:openstack_subnet).instances
+                            .select { |resource| resource[:name] == lookup_id || resource[:id] == lookup_id }
+    return nil if instances.empty?
+
+    instances.first
+  end
+
+  def subnet_resource(lookup_id)
+    lookup_id = lookup_id.is_a?(Array) ? lookup_id.first : lookup_id
+    catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:openstack_subnet)) && [r[:name], r[:id]].include?(lookup_id) }
+  end
+
   # class methods for base class
   module ClassMethods
     def instances
