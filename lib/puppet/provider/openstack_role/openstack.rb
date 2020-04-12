a6a6219eefdf532909f91fe391f71c57c43f22d9
Alexander Ursu <alexander.ursu@gmail.com>
2020-04-13 00:15:25 +0200

    Added openstack_role

---
+++ lib/puppet/provider/openstack_role/openstack.rb
@@ -0,0 +1,76 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'openstack'))
+
+Puppet::Type.type(:openstack_user).provide(:openstack, parent: Puppet::Provider::Openstack) do
+  desc 'manage users for OpenStack.'
+
+  commands openstack: 'openstack'
+
+  # Generates method for all properties of the property_hash
+  mk_resource_methods
+
+  def self.provider_subcommand
+    'role'
+  end
+
+  def self.provider_list
+    get_list(provider_subcommand, 'name', false)
+  end
+
+  def self.provider_create(*args)
+    openstack_caller(provider_subcommand, 'create', *args)
+  end
+
+  def self.provider_delete(*args)
+    openstack_caller(provider_subcommand, 'delete', *args)
+  end
+
+  def self.instances
+    openstack_command
+
+    provider_list.map do |entity_name, entity|
+      new(name: entity_name,
+          ensure: :present,
+          id: entity['id'],
+          domain: nil,
+          provider: name)
+    end
+  end
+
+  def self.prefetch(resources)
+    entities = instances
+    # rubocop:disable Lint/AssignmentInCondition
+    resources.keys.each do |entity_name|
+      if provider = entities.find { |entity| entity.name == entity_name }
+        resources[entity_name].provider = provider
+      end
+    end
+    # rubocop:enable Lint/AssignmentInCondition
+  end
+
+  def create
+    name    = @resource[:name]
+    domain  = @resource.value(:domain)
+
+    @property_hash[:domain] = domain if domain
+
+    args = []
+    args += ['--domain', domain] if domain
+    args << name
+
+    return if self.class.provider_create(*args) == false
+
+    @property_hash[:ensure] = :present
+  end
+
+  def destroy
+    name = @resource[:name]
+
+    self.class.provider_delete(name)
+
+    @property_hash.clear
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present || false
+  end
+end
---
+++ lib/puppet/type/openstack_role.rb
@@ -0,0 +1,29 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..'))
+require 'puppet_x/openstack/customtype'
+
+Puppet::Type.newtype(:openstack_role) do
+  include CustomType
+
+  @doc = <<-PUPPET
+    @summary
+      Like most OpenStack services, keystone protects its API using role-based
+      access control (RBAC).
+
+      Users can access different APIs depending on the roles they have on a
+      project, domain, or system.
+    PUPPET
+
+  ensurable
+
+  newparam(:name, namevar: true) do
+    desc 'New role name'
+  end
+
+  newparam(:id) do
+    desc 'Role ID (read only)'
+  end
+
+  newproperty(:domain) do
+    desc 'Domain the role belongs to (name or ID).'
+  end
+end
