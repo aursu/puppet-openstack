9599e52a4dbf1b28f91d1d0679b8f242bb46f694
Alexander Ursu <alexander.ursu@gmail.com>
2020-07-13 17:08:15 +0200

    CentOS 8 support  into openstack::memcached

--- manifests/memcached.pp
+++ manifests/memcached.pp
@@ -7,8 +7,16 @@
 class openstack::memcached {
   if $::osfamily == 'RedHat' {
     # https://docs.openstack.org/install-guide/environment-memcached-rdo.html
-    package { 'python-memcached':
-      ensure  => 'present',
+    if $facts['os']['release']['major'] == '8' {
+      package { 'python3-memcached':
+        ensure  => 'present',
+      }
+    }
+    else {
+      # https://docs.openstack.org/install-guide/environment-memcached-rdo.html
+      package { 'python-memcached':
+        ensure  => 'present',
+      }
     }
   }
   elsif $::operatingsystem == 'Ubuntu' {
