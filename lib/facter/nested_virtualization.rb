cdd7669efd9718a45a11c4113efc3f8daa55a3c2
Alexander Ursu <alexander.ursu@gmail.com>
2020-09-29 14:58:35 +0200

    corrected return value to boolean values

--- lib/facter/nested_virtualization.rb
+++ lib/facter/nested_virtualization.rb
@@ -1,13 +1,21 @@
 Facter.add(:nested_virtualization) do
   confine { File.exist? '/sys/module/kvm_intel/parameters/nested' }
   setcode do
-    File.read('/sys/module/kvm_intel/parameters/nested') =~ %r{Y|1}
+    if File.read('/sys/module/kvm_intel/parameters/nested') =~ %r{Y|1}
+      true
+    else
+      false
+    end
   end
 end
 
 Facter.add(:nested_virtualization) do
   confine { File.exist? '/sys/module/kvm_amd/parameters/nested' }
   setcode do
-    File.read('/sys/module/kvm_amd/parameters/nested') =~ %r{Y|1}
+    if File.read('/sys/module/kvm_amd/parameters/nested') =~ %r{Y|1}
+      true
+    else
+      false
+    end
   end
 end
