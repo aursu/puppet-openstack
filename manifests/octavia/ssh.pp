fb6744a28c3bd19a39a54049c4e282996e2a997a
Alexander Ursu <alexander.ursu@gmail.com>
2020-11-09 16:51:32 +0100

    Added authentication into openstack_keypair

--- manifests/octavia/ssh.pp
+++ manifests/octavia/ssh.pp
@@ -4,11 +4,19 @@
 #
 # @example
 #   include openstack::octavia::ssh
-class openstack::octavia::ssh {
+class openstack::octavia::ssh (
+  String  $octavia_pass      = $openstack::octavia_pass,
+)
+{
   # Create a key pair for logging in to the amphora instance
   include openssh
 
   $octavia_ssh_dir = '/etc/octavia/.ssh'
+  $auth_octavia = {
+    auth_username     => 'octavia',
+    auth_password     => $octavia_pass,
+    auth_project_name => 'service',
+  }
 
   file { $octavia_ssh_dir:
     ensure => directory,
@@ -23,6 +31,7 @@ class openstack::octavia::ssh {
   }
 
   openstack_keypair { 'octavia_ssh_key':
+    *           => $auth_octavia,
     private_key => "${octavia_ssh_dir}/id_rsa",
     require     => Class['openssh::ssh_keygen'],
   }
