0cd2227ee0e84fbec84a1a49320798b8ae5e7d2b
Alexander Ursu <alexander.ursu@gmail.com>
2022-02-04 00:44:00 +0100

    Ubuntu support for openstack::install

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,38 +11,46 @@ class openstack::install (
 
   openstack::repository { $cycle: }
 
-  if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '8' {
-    # PowerTools repo should be enabled
-    $openstackclient = 'python3-openstackclient'
+  case $facts['os']['name'] {
+    'CentOS': {
+      if $facts['os']['release']['major'] == '8' {
+        $openstackclient = 'python3-openstackclient'
 
-    if $openstack::params::centos_stream {
-      # Network Functions Virtualization (NFV) SIG
-      package { 'centos-release-nfv-openvswitch':
-        ensure => latest,
-        before => Openstack::Repository[$cycle],
+        if $openstack::params::centos_stream {
+          # Network Functions Virtualization (NFV) SIG
+          package { 'centos-release-nfv-openvswitch':
+            ensure => latest,
+            before => Openstack::Repository[$cycle],
+          }
+
+          # in case of upgrade from CentOS 8 to CentOS 8 Stream
+          file { '/etc/dnf/vars/nfvsigdist':
+            ensure  => file,
+            content => "8-stream\n",
+            before  => Openstack::Repository[$cycle],
+          }
+        }
+      }
+      else {
+        $openstackclient = 'python-openstackclient'
       }
 
-      # in case of upgrade from CentOS 8 to CentOS 8 Stream
-      file { '/etc/dnf/vars/nfvsigdist':
-        ensure  => file,
-        content => "8-stream\n",
-        before  => Openstack::Repository[$cycle],
+      package { 'openstack-selinux':
+        ensure  => 'present',
+        require => Openstack::Repository[$cycle],
       }
     }
-  }
-  else {
-    $openstackclient = 'python-openstackclient'
+    'Ubuntu': {
+      $openstackclient = 'python3-openstackclient'
+    }
+    default: {
+      fail("OS is not supported")
+    }
   }
 
   # https://docs.openstack.org/install-guide/environment-packages-rdo.html#finalize-the-installation
-  package {
-    default:
-      ensure  => 'present',
-      require => Openstack::Repository[$cycle],
-    ;
-    $openstackclient:
-    ;
-    'openstack-selinux':
-    ;
+  package { $openstackclient:
+    ensure  => 'present',
+    require => Openstack::Repository[$cycle],
   }
 }
