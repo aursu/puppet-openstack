b64c88719b5b4d8913d961000a45f0ff0435832c
Alexander Ursu <alexander.ursu@gmail.com>
2022-02-04 22:56:33 +0100

    Corrected python client name

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -8,18 +8,20 @@ class openstack::mysql (
   String  $database_tag = $openstack::database_tag,
 )
 {
-
   if $::osfamily == 'RedHat' {
     # https://docs.openstack.org/install-guide/environment-sql-database-rdo.html
-    package { 'python2-PyMySQL':
-      ensure  => 'present',
-    }
+    $python_client = 'python2-PyMySQL'
   }
   elsif $::operatingsystem == 'Ubuntu' {
     # https://docs.openstack.org/install-guide/environment-sql-database-ubuntu.html
-    package { 'python-pymysql':
-      ensure  => 'present',
+    $python_client = $facts['os']['release']['major'] ? {
+      '20.04' => 'python3-pymysql',
+      default => 'python-pymysql',
     }
+  }
+
+  package { $python_client:
+    ensure  => 'present',
   }
 
   # databases
