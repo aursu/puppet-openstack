58be93df74d7bb8fcf68dfeaf85adbcc9bc8b32a
Alexander Ursu <alexander.ursu@gmail.com>
2022-02-18 01:54:08 +0100

    Added facts for Ceph manager host

---
+++ lib/facter/ceph.rb
@@ -0,0 +1,57 @@
+require 'English'
+
+Facter.add(:ceph_conf) do
+  confine { File.exist? '/etc/ceph/ceph.client.admin.keyring' }
+  confine { File.exist? '/etc/ceph/ceph.conf' }
+  setcode do
+    File.read('/etc/ceph/ceph.conf')
+  end
+end
+
+# It is for default cluster name `ceph`
+# based on https://docs.ceph.com/en/latest/rbd/rbd-openstack/
+Facter.add(:ceph_client_glance) do
+  confine { File.exist? '/etc/ceph/ceph.client.admin.keyring' }
+  setcode do
+    client_keyring = `/usr/bin/ceph auth get client.glance` if File.executable?('/usr/bin/ceph')
+
+    if client_keyring && $CHILD_STATUS.success?
+       client_keyring
+    else
+      nil
+    end
+  end
+end
+
+Facter.add(:ceph_client_cinder) do
+  confine { File.exist? '/etc/ceph/ceph.client.admin.keyring' }
+  setcode do
+    client_keyring = `/usr/bin/ceph auth get client.cinder` if File.executable?('/usr/bin/ceph')
+
+    return nil unless client_keyring && $CHILD_STATUS.success?
+
+    client_keyring
+  end
+end
+
+Facter.add(:ceph_client_cinder_backup) do
+  confine { File.exist? '/etc/ceph/ceph.client.admin.keyring' }
+  setcode do
+    client_keyring = `/usr/bin/ceph auth get client.cinder-backup` if File.executable?('/usr/bin/ceph')
+
+    return nil unless client_keyring && $CHILD_STATUS.success?
+
+    client_keyring
+  end
+end
+
+Facter.add(:ceph_ssh_pub_key) do
+  confine { File.exist? '/etc/ceph/ceph.client.admin.keyring' }
+  setcode do
+    pub_key = `/usr/bin/ceph cephadm get-pub-key` if File.executable?('/usr/bin/ceph')
+
+    return nil unless pub_key && $CHILD_STATUS.success?
+
+    pub_key
+  end
+end
