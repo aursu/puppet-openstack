7470a7360bcddc84fa9ba8bb04cd263d236fc41c
Alexander Ursu <alexander.ursu@gmail.com>
2022-02-18 15:30:40 +0100

    Added cephadmin into CLI tools with dependencies

--- manifests/ceph/cli_tools.pp
+++ manifests/ceph/cli_tools.pp
@@ -4,11 +4,46 @@
 #
 # @example
 #   include openstack::ceph::cli_tools
-class openstack::ceph::cli_tools {
+class openstack::ceph::cli_tools (
+  Boolean $manage_epel_repo = true,
+  Boolean $manage_python3   = true,
+  Boolean $manage_podman    = true,
+)
+{
   include openstack::repos::ceph
 
-  package { 'ceph-common':
-    ensure  => present,
-    require => Class['openstack::repos::ceph'],
+  if $manage_podman {
+    include openstack::repos::podman
+
+    package { 'podman':
+      ensure  => present,
+      before  => Package['cephadm'],
+      require => Class['openstack::repos::podman']
+    }
+  }
+
+  if $facts['os']['family'] == 'RedHat' {
+    if $manage_epel_repo {
+      package { 'epel-release':
+        ensure => present,
+        before => Package['cephadm'],
+      }
+    }
+  }
+
+  if $manage_python3 {
+    package { 'python3':
+      ensure => present,
+      before => Package['cephadm'],
+    }
+  }
+
+  package {
+    default:
+      ensure  => present,
+      require => Class['openstack::repos::ceph'],
+    ;
+    'cephadm': ;
+    'ceph-common': ;
   }
 }
