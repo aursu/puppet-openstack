7b6a96e21d02779f011f358cdb63a76fccfe28cd
Alexander Ursu <alexander.ursu@gmail.com>
2022-02-21 09:24:13 +0100

    Replaced openstack::cinder::ceph

--- manifests/ceph/cli_tools.pp
+++ manifests/ceph/cli_tools.pp
@@ -46,4 +46,41 @@ class openstack::ceph::cli_tools (
     'cephadm': ;
     'ceph-common': ;
   }
+
+  # https://docs.ceph.com/en/latest/install/get-packages/
+  $cephadm_home = $facts['os']['family'] ? {
+    'Debian' => '/home/cephadm',
+    default  => '/var/lib/cephadm',
+  }
+
+  group { 'cephadm':
+    ensure => present,
+    system => true,
+  }
+
+  user { 'cephadm':
+    ensure  => present,
+    system  => true,
+    gid     => 'cephadm',
+    comment => 'cephadm user for mgr/cephadm',
+    home    => $cephadm_home,
+    shell   => '/bin/bash',
+    require => Group['cephadm']
+  }
+
+  file {
+    default:
+      owner   => 'cephadm',
+      group   => 'cephadm',
+      require => User['cephadm'],
+    ;
+    [$cephadm_home, "${cephadm_home}/.ssh"]:
+      ensure => directory,
+      mode   => '0700',
+    ;
+    "${cephadm_home}/.ssh/authorized_keys":
+      ensure => file,
+      mode   => '0600',
+    ;
+  }
 }
