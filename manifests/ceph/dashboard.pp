b85355891eabb2a6a0a1be5b18ef7ae83f6303f7
Alexander Ursu <alexander.ursu@gmail.com>
2022-02-22 01:45:40 +0100

    Setup certificate/key into dashboard config

--- manifests/ceph/dashboard.pp
+++ manifests/ceph/dashboard.pp
@@ -28,4 +28,25 @@ class openstack::ceph::dashboard (
     $ssl_cert = undef
     $ssl_key  = undef
   }
+
+  if $ssl_cert and $ssl_key {
+    exec { 'ceph-dashboard-set-ssl-certificate':
+      command     => "ceph dashboard set-ssl-certificate -i ${ssl_cert}",
+      path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+      onlyif      => "test -f ${ssl_cert}",
+      refreshonly => true,
+    }
+
+    exec { 'ceph-dashboard-set-ssl-certificate-key':
+      command     => "ceph dashboard set-ssl-certificate-key -i ${ssl_key}",
+      path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+      onlyif      => "test -f ${ssl_key}",
+      refreshonly => true,
+    }
+
+    if $cert_identity =~ String and $manage_certificate {
+      Tlsinfo::Certpair[$cert_identity] ~> Exec['ceph-dashboard-set-ssl-certificate']
+      Tlsinfo::Certpair[$cert_identity] ~> Exec['ceph-dashboard-set-ssl-certificate-key']
+    }
+  }
 }
